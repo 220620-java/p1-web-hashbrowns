53d6cbc8b4863f8ca481ca8d99080212193f72b2
tonywied17 <107732340+tonywied17@users.noreply.github.com>
2022-07-16 22:28:26 -0400

    F

--- rest/src/main/java/p1/servlets/HelloServlet.java
+++ rest/src/main/java/p1/servlets/HelloServlet.java
@@ -9,11 +9,13 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-// our servlets should extend HttpServlet
 public class HelloServlet extends HttpServlet {
-	// the servlet container (tomcat catalina) will call this method when
-	// a GET request comes in to the right path and it will create the objects
-	// for the request and response and pass those in
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// gets the response body writer object so that we can write to the response
--- rest/src/main/java/p1/servlets/PlayerServlet.java
+++ rest/src/main/java/p1/servlets/PlayerServlet.java
@@ -1,9 +1,12 @@
 package p1.servlets;
 
 import java.io.BufferedReader;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import hashbrowns.p1.orm.mapper.OrmExec;
+import hashbrowns.p1.orm.mapper.OrmService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,61 +14,64 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class PlayerServlet extends HttpServlet {
 
+	OrmService ormM = new OrmExec();
 
-		@Override
-		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-			StringBuilder uriString = new StringBuilder(req.getRequestURI()); // /pet-app1/hello/6
-			uriString.replace(0, req.getContextPath().length()+1, ""); // hello/6
-			
-			// if there is a slash
-			if (uriString.indexOf("/") != -1) {
-				
-				uriString.replace(0, uriString.indexOf("/")+1, ""); 
-				
-				String[] numbers = uriString.toString().split(" ");
-				for (String number : numbers) {
-				    System.out.println(Integer.valueOf(number)); 
-				}
-				
-				PrintWriter writer = resp.getWriter();
-				writer.write(uriString.toString());
-			} else {
-				// gets the response body writer object so that we can write to the response body
-				PrintWriter writer = resp.getWriter();
-				writer.write("Players");
-			}
-		}
-		
-		@Override
-		protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		StringBuilder uriString = new StringBuilder(req.getRequestURI()); // /pet-app1/hello/6
+		uriString.replace(0, req.getContextPath().length() + 1, ""); // hello/6
 
-			String language = req.getParameter("language");
-			
-			// this gets a reader that will read the HTTP request body
-			BufferedReader reader = req.getReader();
-		
-			String requestBody = "";
-			String line = "";
-			while ((line=reader.readLine())!=null) {
-				requestBody += line;
+		// if there is a slash
+		if (uriString.indexOf("/") != -1) {
+
+			uriString.replace(0, uriString.indexOf("/") + 1, "");
+
+			String[] numbers = uriString.toString().split(" ");
+			for (String number : numbers) {
+				System.out.println(Integer.valueOf(number));
 			}
-			
+
 			PrintWriter writer = resp.getWriter();
-			
-			if (language==null) language="";
-			switch (language) {
-			case "en":
-				writer.write("Hello, " + requestBody + "! :)");
-				break;
-			case "fr":
-				writer.write("Bonjour, " + requestBody + "! :)");
-				break;
-			case "es":
-				writer.write("Hola, " + requestBody + "! :)");
-				break;
-			default:
-				writer.write(requestBody + "! :)");
-			}
-			
+			writer.write(uriString.toString());
+		} else {
+			// gets the response body writer object so that we can write to the response
+			// body
+			PrintWriter writer = resp.getWriter();
+			writer.write("Players");
 		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		String language = req.getParameter("language");
+
+		// this gets a reader that will read the HTTP request body
+		BufferedReader reader = req.getReader();
+
+		String requestBody = "";
+		String line = "";
+		while ((line = reader.readLine()) != null) {
+			requestBody += line;
+		}
+
+		PrintWriter writer = resp.getWriter();
+
+		if (language == null)
+			language = "";
+		switch (language) {
+		case "en":
+			writer.write("Hello, " + requestBody + "! :)");
+			break;
+		case "fr":
+			writer.write("Bonjour, " + requestBody + "! :)");
+			break;
+		case "es":
+			writer.write("Hola, " + requestBody + "! :)");
+			break;
+		default:
+			writer.write(requestBody + "! :)");
+		}
+
+	}
 }
